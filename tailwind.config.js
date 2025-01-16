e24d3280883044ca1340750153c3fda9e98074e1
crepefeu <esteban.rodriguez@live.fr>
2025-01-16 09:31:15 +0100

    Add Webpack Encore and Tailwind CSS configuration; update .gitignore and package.json

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./assets/**/*.jsx",
+    "./assets/**/**/*.jsx",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
