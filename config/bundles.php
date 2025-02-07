97a3ba45fdf0410524e6e8cc2336dbfd24d78261
crepefeu <esteban.rodriguez@live.fr>
2025-02-07 12:36:13 +0100

    Update DoctrineFixturesBundle configuration; enable for all environments

--- config/bundles.php
+++ config/bundles.php
@@ -12,7 +12,7 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\UX\React\ReactBundle::class => ['all' => true],
